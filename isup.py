1f556d88218d32691b33bf2a38a2b3d61deb83cb
pkorba <32198274+pkorba@users.noreply.github.com>
2025-06-13 16:42:35 +0200

    visual refresh

--- isup.py
+++ isup.py
@@ -5,24 +5,24 @@ from urllib.parse import urlparse, ParseResult
 
 
 class IsUpBot(Plugin):
-    @command.new(name="isup", help="check if website is online")
-    @command.argument("message", pass_raw=True, required=True)
-    async def isup(self, evt: MessageEvent, message: str) -> None:
+    @command.new(name="isup", help="Check if a website is online")
+    @command.argument("url", pass_raw=True, required=True)
+    async def isup(self, evt: MessageEvent, url: str) -> None:
         await evt.mark_read()
-        message = message.strip()
-        if not message:
-            await evt.respond("Usage: !isup <url>")
+        url = url.strip()
+        if not url:
+            await evt.reply("> **Usage:** !isup <url>")
             return
-        url = await self.parse_url(message)
+        url = await self.parse_url(url)
         if not url.hostname:
-            await evt.reply("Incorrect URL.")
+            await evt.reply("> Incorrect URL.")
             return
 
         headers = {
             "Sec-GPC": "1",
             "accept-encoding": "gzip, deflate, br, zstd",
-            "accept-language": "pl,en-US;q=0.7,en;q=0.3",
-            "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:138.0) Gecko/20100101 Firefox/138.0"
+            "accept-language": "en,en-US;q=0.5",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:139.0) Gecko/20100101 Firefox/139.0"
         }
         try:
             full_url = url._replace(path="", params="", query="", fragment="").geturl()
@@ -30,18 +30,18 @@ class IsUpBot(Plugin):
             async with aiohttp.ClientSession(timeout=timeout, headers=headers) as session:
                 async with session.get(full_url) as response:
                     if response.status < 400:
-                        await evt.reply(f"{url.hostname} is up.")
+                        await evt.reply(f"> {url.hostname} is up.")
                     else:
-                        await evt.reply(f"{url.hostname} is up, but server returns an error.")
+                        await evt.reply(f"> {url.hostname} is up, but server returns an error.")
         except TimeoutError:
-            await evt.reply(f"{url.hostname} is down.")
+            await evt.reply(f"> {url.hostname} is down.")
         except aiohttp.ClientError as e:
             self.log.error(f"Connection failed: {url.hostname}: {e}")
-            await evt.reply(f"Failed to check status of {url.hostname}. Connection error.")
+            await evt.reply(f"> Failed to check status of {url.hostname}. Connection error.")
 
     @staticmethod
-    async def parse_url(message: str) -> ParseResult:
-        if not message.lower().startswith("http://") and not message.lower().startswith("https://"):
-            message = f"https://{message}"
-        url = urlparse(message)
-        return url
+    async def parse_url(url: str) -> ParseResult:
+        if not url.lower().startswith("http://") and not url.lower().startswith("https://"):
+            url = f"https://{url}"
+        new_url = urlparse(url)
+        return new_url
